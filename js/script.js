c593399b7b68b7561ed0106395ea8eacea070daf
Aguilar <luis.aguilar.bolanos@est.una.ac.cr>
2023-04-22 13:17:22 -0600

    Paginas Inicio y HTML finalizadas
    Añadido contenido y configurado css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function gotopage(x){
+    if(x==1){
+        window.location.href = "html/html.html";
+    }
+    if(x==2){
+        window.location.href = "html/css.html";
+    }
+    if(x==3){
+        window.location.href = "html/js.html";
+    }
+}
